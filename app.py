dff6f1d156907704f218562be13fdb5b76292f86
Mariraja S. Selvaraja <mariraja_s@solartistech.local>
2022-01-25 15:31:23 +0530

    Commited

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-       render_template('index.html')
+       return "IiWEB TEZ"
 if __name__ == '__main__':
 	app.run()
 
