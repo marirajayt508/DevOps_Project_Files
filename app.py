99d59863f0e06d1cedc05bd88ba16b48b27ac815
Mariraja S. Selvaraja <mariraja_s@solartistech.local>
2022-01-25 15:42:48 +0530

    Commited

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-       return "IiWEB TEZ"
+       return "I am Mariraja"
 if __name__ == '__main__':
 	app.run()
+
 
 
 
