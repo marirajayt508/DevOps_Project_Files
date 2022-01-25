40ce80726e06704c5b5b4e11008918f4fefda785
Mariraja S. Selvaraja <mariraja_s@solartistech.local>
2022-01-25 14:14:58 +0530

    {cmt_msg}

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-	return 'I am Mariraja Selvaraja...'
+	return 'iWEB TEZ'
 if __name__ == '__main__':
 	app.run()
 
