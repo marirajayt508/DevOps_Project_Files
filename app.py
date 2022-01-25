7fb2306312e21257564f5d3699b744f785a4abe3
Mariraja S. Selvaraja <mariraja_s@solartistech.local>
2022-01-25 14:19:40 +0530

    'Commited'

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-	return 'iWEB TEZ CEO is Mariraja Selvaraja.'
+	return 'iWEB TEZ CEO is Mariraja Selvaraja..'
 if __name__ == '__main__':
 	app.run()
+
 
 
 
